8609fac2e685c617968725abc9b55036935b0e7c
my3eyereality-stack <my3eyereality@gmail.com>
2026-04-09 16:02:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { tgReady, tgExpand } from './tg.js'
-
-tgReady()
-tgExpand()
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+import { initTg } from './tg.js'
+initTg()
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
